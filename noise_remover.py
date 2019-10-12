edb3178e39d6f490a998d10f942306bd06b4d4af
2019-Q2_SOW_DEV_031 <2019-Q2_SOW_DEV_031@gmail.com>
2019-10-12 13:49:06 +0200

    removed comments

--- noise_remover.py
+++ noise_remover.py
@@ -9,16 +9,13 @@ def perf_noise_cancelling(content):
     words = nltk.word_tokenize(content)
     without_stopwords = []
 
-    # filter
     for word in words:
         if word not in stopwords.words('english') and word.isalpha():
             without_stopwords.append(word)
 
-    # display
     print("Without stop words: ", without_stopwords)
 
 
-# tokenize
 def main():
     content = read_file("./resources/titanic.txt", "r")
     perf_noise_cancelling(content)
