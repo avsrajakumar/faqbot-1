0392f8f685e10d7934f36a54a60da1e6dd25d3ad
2019-Q2_SOW_DEV_031 <2019-Q2_SOW_DEV_031@gmail.com>
2019-10-12 13:46:00 +0200

    defined noise cancelling implementation as a function to reuse

--- noise_remover.py
+++ noise_remover.py
@@ -4,19 +4,25 @@ nltk.download("stopwords")
 from nltk.corpus import stopwords
 from file_reader import read_file
 
-# read file
-file = read_file("./resources/titanic.txt", "r")
-content = file.read()
-print("Content: ", content)
+
+def main():
+    content = read_file("./resources/titanic.txt", "r")
+    perf_noise_cancelling(content)
+
 
 # tokenize
-words = nltk.word_tokenize(content)
-without_stopwords = []
+def perf_noise_cancelling(content):
+    words = nltk.word_tokenize(content)
+    without_stopwords = []
 
-# filter
-for word in words:
-    if word not in stopwords.words('english') and word.isalpha():
-        without_stopwords.append(word)
+    # filter
+    for word in words:
+        if word not in stopwords.words('english') and word.isalpha():
+            without_stopwords.append(word)
 
-# display
-print("Without stop words: ", without_stopwords)
+    # display
+    print("Without stop words: ", without_stopwords)
+
+
+if __name__ == '__main__':
+    main()
