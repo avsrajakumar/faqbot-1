64062320fd5765bf722f0818466255b80bc84228
2019-Q2_SOW_DEV_031 <2019-Q2_SOW_DEV_031@gmail.com>
2019-10-12 13:36:45 +0200

    defined file reading implementation as a method to reuse

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,13 @@
-file = open("./resources/titanic.txt", "r")
-content = file.read()
-print(content)
-file.close()
+def readFile(source, mode):
+    file = open(source, mode)
+    content = file.read()
+    file.close()
+    return content
+
+
+def main():
+    print(readFile("./resources/titanic.txt", "r"))
+
+
+if __name__ == '__main__':
+    main()
