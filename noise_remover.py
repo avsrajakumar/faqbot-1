48289efa685d8aa58bfdb48285770ea7a917496d
2019-Q2_SOW_DEV_031 <2019-Q2_SOW_DEV_031@gmail.com>
2019-10-12 13:26:51 +0200

    implemented removing stop words in a given text content

---
+++ noise_remover.py
@@ -0,0 +1,21 @@
+import nltk
+
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+
+# read file
+file = open("./resources/titanic.txt", "r")
+content = file.read()
+print("Content: ", content)
+
+# tokenize
+words = nltk.word_tokenize(content)
+without_stopwords = []
+
+# filter
+for word in words:
+    if word not in stopwords.words('english'):
+        without_stopwords.append(word)
+
+# display
+print("Without stop words: ", without_stopwords)
